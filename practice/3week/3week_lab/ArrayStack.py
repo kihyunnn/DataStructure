61d6833b1e6ab5996231a128e516d43ea0dff6af
kihyunnn <mable0927@gmail.com>
2025-03-20 15:30:56 +0900

    SLOVE:과제1번 스택 구현 완료

--- practice/3week/3week_lab/ArrayStack.py
+++ practice/3week/3week_lab/ArrayStack.py
@@ -6,20 +6,21 @@ class ArrayStack:
         
     def is_empty(self):
         ''' 1) isEmpty() 함수를 마저 작성하시오. '''
-        return ###  block  ###
+        return self.top == 0 #비어있으면 True, 안비어있으면 False 반환
+    ###  block  ###
     
     def is_full(self):
         ''' 2) isFull()의 함수를 마저 작성하시오. '''
-        return ###  block  ###
+        return self.top == self.max_stack_size #꽉 차있으면 True, 안차있으면 False 반환
+    ###  block  ###
     
     def push(self, e):
         if self.is_full():
             print("스택 포화 에러")
-            return
+            return None
         ''' 3) push의 함수를 마저 작성하시오. '''
-        #####################################
-        #               block               #
-        #####################################
+        self.data.append(e)
+        self.top += 1
     
     def pop(self):
         if self.is_empty():
@@ -30,6 +31,8 @@ class ArrayStack:
         #####################################
         #               block               #
         #####################################
+        self.top -=1
+        return self.data[self.top]
     
     def peek(self):
         if self.is_empty():
@@ -37,12 +40,17 @@ class ArrayStack:
             return None
         
         ''' 5) peek의 함수를 마저 작성하시오. '''
-        return ###  block  ###
+        return self.data[self.top-1]
+    ###  block  ###
     
     def display(self):
         print(f"[스택 항목의 = {self.top}] ==> ", end="")
         
         ''' 6) test의 결과를 보고, 올바른 출력이 나올 수 있도록 for문을 완성하시오. '''
-        for ###  block  ###:
+        for i in range(self.top):###  block  ###
             print(f"data[{i}]: {self.data[i]} ", end="")
         print()
+        
+        
+
+
